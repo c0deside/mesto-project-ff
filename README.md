1819c9cefdce29ff6d32ab2b463785996082148a
Mikhail Rumyantsev <m.rumyantsev@codeside.ru>
2025-02-01 22:34:20 +0300

    Add cards and card removing

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Проектная работа Mesto
 
+## Ссылка не репозиторий
+
+https://github.com/c0deside/mesto-project-ff
+
+## Ссылка на макет
+
+https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+function createCard(name, link, removeCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__image').src = link;
+  card.querySelector('.card__image').alt = name;
+  card.querySelector('.card__title').textContent = name;
 
-// @todo: Функция создания карточки
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => removeCard(deleteButton));
 
-// @todo: Функция удаления карточки
+  return card;
+}
 
-// @todo: Вывести карточки на страницу
+function removeCard(button) {
+  button.closest('.card').remove();
+}
+
+const placesListElement = document.querySelector('.places__list');
+placesListElement.append(...initialCards.map(card => createCard(card.name, card.link, removeCard)));
