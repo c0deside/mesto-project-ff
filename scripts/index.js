268ca411f35b3b07826b4dbfead3cd2440dfea21
Mikhail Rumyantsev <m.rumyantsev@codeside.ru>
2025-02-02 07:47:30 +0300

    Optimize queries

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,23 @@
+const placesListElement = document.querySelector('.places__list');
+
 function createCard(name, link, removeCard) {
   const cardTemplate = document.querySelector('#card-template').content;
 
   const card = cardTemplate.querySelector('.card').cloneNode(true);
-  card.querySelector('.card__image').src = link;
-  card.querySelector('.card__image').alt = name;
+
+  const cardImage = card.querySelector('.card__image');
+  cardImage.src = link;
+  cardImage.alt = name;
+
   card.querySelector('.card__title').textContent = name;
 
-  const deleteButton = card.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', () => removeCard(deleteButton));
+  card.querySelector('.card__delete-button').addEventListener('click', () => removeCard(card));
 
   return card;
 }
 
-function removeCard(button) {
-  button.closest('.card').remove();
+function removeCard(card) {
+  card.remove();
 }
 
-const placesListElement = document.querySelector('.places__list');
 placesListElement.append(...initialCards.map(card => createCard(card.name, card.link, removeCard)));
